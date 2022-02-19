fdec5e8aa923f3e1e1fa4251fc2d1c808a63f79c
GuilhermeJM <gjm@outlook.com>
2022-02-19 16:05:45 -0300

    Projeto criado

---
+++ src/main/java/com/gjm/gjmproj/GjmprojApplication.java
@@ -0,0 +1,13 @@
+package com.gjm.gjmproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjmprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjmprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gjm/gjmproj/GjmprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjm.gjmproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjmprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
