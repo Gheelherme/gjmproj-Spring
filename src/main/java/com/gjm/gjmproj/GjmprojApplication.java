0d8d3e04844aefe6423a4736bd46963522fd3854
GuilhermeJM <gjm@outlook.com>
2022-03-15 18:57:49 -0300

    Criando operacao de instanciacao

--- src/main/java/com/gjm/gjmproj/GjmprojApplication.java
+++ src/main/java/com/gjm/gjmproj/GjmprojApplication.java
@@ -1,13 +1,33 @@
 package com.gjm.gjmproj;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gjm.gjmproj.domain.Categoria;
+import com.gjm.gjmproj.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class GjmprojApplication {
+public class GjmprojApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GjmprojApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
